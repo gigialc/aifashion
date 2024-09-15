78a3b63b0c00a158cb71fefa50862b2ae86c7002
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-09-15 10:36:57 -0400

    hi

--- app/page.js
+++ app/page.js
@@ -159,7 +159,7 @@ const ChicChat = () => {
     <div className="min-h-screen bg-cover bg-center bg-no-repeat p-8 font-sans mb-20" style={{backgroundImage: "url('/hackmnit.jpg')"}}>
       <div className="max-w-4xl mx-auto">
         <h1 className="text-6xl text-center mb-2 text-black tracking-tight font-serif leading-tight transform scale-105 transition-all duration-500 hover:text-gray-800">
-          MITstyle
+         MyAIStylist
         </h1>
         <p className="text-center mb-8 text-gray-600 text-xl">your personal ai stylist</p>
 

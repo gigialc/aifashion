bd8eb6eef7b5413dee5752261c6bd83c27b3cb84
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-09-15 03:55:38 -0400

    hi

--- app/page.js
+++ app/page.js
@@ -103,7 +103,7 @@ const ChicChat = () => {
   };
 
   return (
-    <div className="min-h-screen bg-cover bg-center bg-no-repeat p-8 font-sans" style={{backgroundImage: "url('/hackmnit.jpg')"}}>
+    <div className="min-h-screen bg-cover bg-center bg-no-repeat p-8 font-sans mb-20" style={{backgroundImage: "url('/hackmnit.jpg')"}}>
       <div className="max-w-4xl mx-auto">
         <h1 className="text-6xl text-center mb-2 text-black tracking-tight font-serif leading-tight transform scale-105 transition-all duration-500 hover:text-gray-800">
           fashion
